9d8fe823f9e186bb211d0d3513c175b5955f7c7e
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2026-01-28 17:21:50 +0530

    Write merging_data_with_pandas.py

---
+++ merging_data_with_pandas.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import streamlit as st
+
+customer_df = pd.read_csv("customers.csv")
+orders_df = pd.read_csv("orders.csv")
+
+customer_df.set_index("customer_id", inplace=True)
+orders_df.set_index("order_id", inplace=True)
+
+st.title("Merging Data with Pandas")
+
+st.subheader("Customer Dataset header")
+st.dataframe(customer_df.head())
+
+st.subheader("Orders Dataset header")
+st.dataframe(orders_df.head())
+
+inner_joined_df = pd.merge(
+    customer_df.reset_index(),
+    orders_df.reset_index(),
+    on="customer_id",
+    how="inner"
+)
+
+left_joined_df = pd.merge(
+    customer_df.reset_index(),
+    orders_df.reset_index(),
+    on="customer_id",
+    how="left"
+)
+
+st.header("Merged Dataset")
+
+st.subheader("Inner Joined Dataset")
+st.dataframe(inner_joined_df)
+
+st.subheader("Left Joined Dataset")
+st.dataframe(left_joined_df)
+
+# Missing relationships
+st.header("Missing Relationships")
+
+customers_without_orders = left_joined_df[left_joined_df["order_id"].isna()]
+st.subheader("Customers without Orders")
+st.dataframe(customers_without_orders)
+
+orders_without_customers = orders_df[~orders_df["customer_id"].isin(customer_df.index)]
+st.subheader("Orders without Customers")
+st.dataframe(orders_without_customers)
